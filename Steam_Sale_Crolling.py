2f725f01d324db248690e865b2f572373097b044
yofp1937 <yofp1937@naver.com>
2023-07-15 13:48:57 +0900

    세일 게임 크롤링 완성

--- Steam_Sale_Crolling.py
+++ Steam_Sale_Crolling.py
@@ -5,8 +5,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-
+import openpyxl
+from pathlib import Path
+from io import BytesIO
+import dload
 from time import sleep
+from datetime import datetime
+
+#현재시간 변수에 저장(파일경로와 파일이름에 현재시간을 넣기위해)
+time = datetime.now()
+timestr = time.strftime("%Y%m%d_%H%M")
 
 #스팀 상점 URL 설정
 URL = 'https://store.steampowered.com/specials/'
@@ -21,6 +29,7 @@ driver.set_window_size(1400,1000)
 driver.get(URL)
 driver.implicitly_wait(10)
 
+#ActionChains 사용하기위해 action 미리 지정
 action = ActionChains(driver)
 
 #현재 페이지 주소 html에 설정, BeautilfulSoup으로 페이지 parsing
@@ -31,8 +40,8 @@ soup = BeautifulSoup(html, "html.parser")
 steam_language = soup.find("span", {"class": "pulldown global_action_link"})
 
 if steam_language.string != "언어":
-    driver.find_element(By.XPATH, "/html/body/div[1]/div[7]/div[3]/div/div[3]/div/span").click()
-    driver.find_element(By.XPATH, "/html/body/div[1]/div[7]/div[3]/div/div[3]/div/div/div/a[4]").click()
+    driver.find_element(By.CSS_SELECTOR, "#language_pulldown").click()
+    driver.find_element(By.CSS_SELECTOR, "#language_dropdown > div > a:nth-child(4)").click()
 sleep(5)
 
 
@@ -46,18 +55,6 @@ driver.execute_script('return window.pageYOffset')
 
 # 지정 좌표로 스크롤 이동
 driver.execute_script('window.scrollTo(x, y)')
-
-#스크롤을 제일 아래까지 내리는 제어 반복문 // steam에서 스크롤이 반환받은 scrollHeigth까지 안내려가서 폐기
-scroll_location = driver.execute_script('return document.body.scrollHeight')
-while True:
-    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-    sleep(1.5)
-    scroll_height = driver.execute_script('return window.pageYOffset')
-
-    if scroll_location == scroll_height:
-        break
-    else:
-        scroll_location = driver.execute_script('return document.body.scrollHeight')
 """
 
 #스크롤을 최하단까지 중간에 1.5초의 텀을주고 두번 내려서 동적으로 생성되는 객체들을 불러온후 스크롤을 최하단으로 내림
@@ -77,13 +74,47 @@ while True:
         action.move_to_element(button).perform()
         #'더 보기' 버튼을 클릭한다
         button.click()
-    else: #'더 보기' 버튼이 존재하면 break로 반복문 빠져나옴
+    else: #'더 보기' 버튼이 존재하지않으면 break로 반복문 빠져나옴
         break
 
+#더보기 다누르면 화면 맨아래로 내려서 동적데이터 전부 생성한후 다시 페이지 파싱
+sleep(1.5)
+driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
 
-#할인 상품전체 패널
-panel = soup.find("div", {"class": "partnersaledisplay_SaleSection_2NfLq eventbbcodeparser_SaleSectionCtn_2Xrw_ SaleSectionForCustomCSS"})
+#액셀 인스턴스 생성, 경로 지정
+excel_File = openpyxl.Workbook()
+excel_sheet = excel_File.active
 
+row_column = ["순위", "게임명", "정가", "할인가", "할인율", "파일 이미지"]
+excel_sheet.append(row_column)
+
+path = f'D:\Python\Study\SteamSale\{timestr}'
+Path(path).mkdir(parents=True, exist_ok=True)
+
+#게임 데이터 크롤링
+panel = soup.select_one("div.saleitembrowser_SaleItemBrowserContainer_2wLns")
+gamelist = panel.select("div.salepreviewwidgets_SaleItemBrowserRow_y9MSd")
+
+i = 1
+for item in gamelist:
+    title = item.find("div", class_="salepreviewwidgets_StoreSaleWidgetTitle_3jI46 StoreSaleWidgetTitle")
+    price = item.find("div", class_="salepreviewwidgets_StoreOriginalPrice_1EKGZ")
+    saleprice = item.find("div", class_="salepreviewwidgets_StoreSalePriceBox_Wh0L8")
+    saleper = item.find("div", class_="salepreviewwidgets_StoreSaleDiscountBox_2fpFv")
+    img = item.find("img", class_="salepreviewwidgets_CapsuleImage_cODQh")
+    imgdata = img["src"]
+
+    #액셀에 데이터 집어넣기, 이미지 저장
+    data_column = [i, title.text, price.text, saleprice.text, saleper.text, imgdata]
+    excel_sheet.append(data_column)
+    dload.save(imgdata, f'{path}\IMG_{i}.jpg')
+    i += 1
+
+#액셀 저장 후 종료
+excel_File.save(f'{path}\SteamSale_{timestr}.xlsx')
+excel_File.close()
 
 #모든 작업이 완료되면 5초 대기했다가 인스턴스 해제
 sleep(5)
