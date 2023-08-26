c4adb29a6f3dbe88e9c4b01845c157edbaabb1b7
dbwldk <yujiayujia55@yuhan.ac.kr>
2023-08-26 18:46:12 +0900

    GameDTO 수정
    게임 데이터 불러오기 용(테스트)

--- loco/src/main/java/com/yuhan/loco/game/GameDTO.java
+++ loco/src/main/java/com/yuhan/loco/game/GameDTO.java
@@ -1,44 +1,37 @@
 package com.yuhan.loco.game;
 
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Size;
-
 
 public class GameDTO {
-	//
-	@Id
-	private int seq;
-	
-	@Size(max = 45)
+	private int ranking;
 	private String gameName;
-	
 	private String gameUrl;
-
 	
 	//getter setter
-	public int getSeq() {
-		return seq;
+	public int getRanking() {
+		return ranking;
 	}
-
-	public void setSeq(int seq) {
-		this.seq = seq;
+	public void setRanking(int ranking) {
+		this.ranking = ranking;
 	}
-
 	public String getGameName() {
 		return gameName;
 	}
-
 	public void setGameName(String gameName) {
 		this.gameName = gameName;
 	}
-
 	public String getGameUrl() {
 		return gameUrl;
 	}
-
 	public void setGameUrl(String gameUrl) {
 		this.gameUrl = gameUrl;
 	}
 	
+	//생성자
+	public GameDTO(int ranking, String gameName, String gameUrl) {
+		super();
+		this.ranking = ranking;
+		this.gameName = gameName;
+		this.gameUrl = gameUrl;
+	}
 	
 }
