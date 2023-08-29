3bfd29c8a718f15647de599ed59b4830438b25b2
yofp1937 <yofp1937@naver.com>
2023-08-29 19:58:34 +0900

    스팀, 에픽게임즈 DB에 저장

--- TestFile/EpicGames_BestPlaying.py
+++ TestFile/EpicGames_BestPlaying.py
@@ -1,44 +1,158 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
-import openpyxl
-from pathlib import Path
-from io import BytesIO
-import dload
+#import openpyxl
+#from pathlib import Path
+#from io import BytesIO
+#import dload
 from time import sleep
-from datetime import datetime
+#from datetime import datetime
+import MySQLdb
+import subprocess
+import requests
 
-time = datetime.now()
-timestr = time.strftime("%Y%m%d_%H%M")
+def find_element_by_css_selector(driver, css_selector):
+    try:
+        return driver.find_element(By.CSS_SELECTOR, 'css_selector')
+    except NoSuchElementException as e:
+        return None
+
+#DB연결
+conn = MySQLdb.connect(
+    user="crawler",
+    passwd="crawler1937",
+    host="localhost",
+    db="crawling_test"
+)
+cursor = conn.cursor()
+
+# 실행할 때마다 다른값이 나오지 않게 테이블을 제거해두기
+cursor.execute("DROP TABLE IF EXISTS epic_best_playing_genre")
+cursor.execute("DROP TABLE IF EXISTS epic_best_playing")
+
+#steam_best 테이블 생성
+#rank에 AUTO_INCREMENT를 사용함으로써 INSERT가 입력될때마다 자동으로 숫자를 +1 올린다
+cursor.execute('''CREATE TABLE epic_best_playing (
+                                           num int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                           title varchar(100) NOT NULL UNIQUE KEY,
+                                           price varchar(30) NOT NULL,
+                                           saleprice varchar(30),
+                                           saleper varchar(30),
+                                           description varchar(500),
+                                           imgdata varchar(3000)
+                                           )
+                                           ''')
+
+cursor.execute('''CREATE TABLE epic_best_playing_genre (
+                                           num INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                           title varchar(100) NOT NULL,
+                                           genre varchar(20) NOT NULL,
+                                           FOREIGN KEY(title) REFERENCES epic_best_playing(title)
+                                           )
+                                           ''')
 
 URL = 'https://store.epicgames.com/ko/collection/most-played'
 
+subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie"')
+
 #크롬드라이버 옵션 설정
+services = Service(executable_path=ChromeDriverManager().install())
 options = Options()
-options.add_experimental_option("detach", True)
+options.add_experimental_option("debuggerAddress", '127.0.0.1:9222')
 
 #크롬드라이버 인스턴스 생성 및 옵션, 크기, URL, 암시적 대기 설정
-driver = webdriver.Chrome(options=options)
-driver.set_window_size(1400,1000)
+driver = webdriver.Chrome(service=services, options=options)
+sleep(3)
+driver.implicitly_wait(5)
+driver.set_window_size(1400,800)
 driver.get(URL)
-driver.implicitly_wait(10)
 
-#ActionChains 사용하기위해 action 미리 지정
-action = ActionChains(driver)
-
+soup = BeautifulSoup(driver.page_source, "html.parser")
 page = driver.find_element(By.TAG_NAME, "body")
 
 for _ in range(0,20):
     page.send_keys(Keys.PAGE_DOWN)
+    sleep(0.5)
 
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
+#게임 데이터 크롤링
+panel = soup.select_one("section.css-zjpm9r")
+gamelist = panel.select("div.css-lrwy1y")
+
+for item in gamelist:
+    title = item.find("div", class_="css-rgqwpc")
+
+    if title == None:
+        title = item.find("div",class_="css-8uhtka").text
+    else:
+        title = title.text
+
+    price = item.find("span", class_="css-119zqif")
+    if price == None:
+        price = "정보 없음"
+    else:
+        price = price.text
+    
+    saleper = item.find("div", class_="css-b0xoos")
+    if saleper != None:
+        price = item.find("div", class_="css-4jky3p").text
+        saleprice = item.find("span", class_="css-119zqif").text
+        saleper = saleper.text
+    else:
+        saleper = "X"
+        saleprice = "X"
+    
+    img = item.find("img", class_="css-174g26k")
+    imgdata = img["src"]
+
+    #move 변수에 해당 게임 페이지 링크 획득
+    game_link = item.select_one('a.css-g3jcms')
+    move = game_link["href"]
+
+    #게임 페이지로 새탭에서 열기
+    driver.execute_script(f'window.open(\'{move}\');')
+    #새탭으로 이동
+    driver.switch_to.window(driver.window_handles[-1])
+
+    new_soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    description = new_soup.select_one("div.css-1myreog")
+    if description != None:
+        description = description.text.strip()
+    tag = new_soup.select("li.css-t8k7")
+
+    tag_length = len(tag)
+    num = 0
+
+    sql = 'INSERT INTO epic_best_playing (title, price, saleprice, saleper, description, imgdata) VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(sql, (title, price, saleprice, saleper, description, imgdata))
+
+    while num < tag_length:
+        tag[num] = tag[num].text.strip()
+        sql = 'INSERT INTO epic_best_playing_genre (title, genre) VALUES (%s, %s)'
+        cursor.execute(sql, (title, tag[num]))
+        num += 1
+
+    conn.commit()
+    print(f'{title} db에 입력완료')
+
+    #탭 종료후 원래 탭으로 이동
+    driver.close()
+    driver.switch_to.window(driver.window_handles[-1])
+
+sleep(2)
+print("크롤링 완료")
+driver.close()
+
+"""
 #액셀 인스턴스 생성, 경로 지정
 excel_File = openpyxl.Workbook()
 excel_sheet = excel_File.active
@@ -48,10 +162,6 @@ excel_sheet.append(row_column)
 
 path = f'D:\Python\Study\Epic_BestPlaying{timestr}'
 Path(path).mkdir(parents=True, exist_ok=True)
-
-#게임 데이터 크롤링
-panel = soup.select_one("section.css-zjpm9r")
-gamelist = panel.select("div.css-lrwy1y")
 
 i = 1
 
@@ -88,3 +198,4 @@ excel_File.close()
 
 #모든 작업이 완료되면 5초 대기했다가 인스턴스 해제
 driver.quit()
+"""
--- TestFile/Steam_best_game.py
+++ TestFile/Steam_best_game.py
@@ -1,29 +1,66 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 from time import sleep
-from datetime import datetime
-import openpyxl
-from io import BytesIO
-import dload
-from pathlib import Path
+#from datetime import datetime
+#import openpyxl
+#from io import BytesIO
+#import dload
+#from pathlib import Path
+import MySQLdb
+import requests
 
-time = datetime.now()
-timestr = time.strftime("%Y%m%d_%H%M")
+#DB연결
+conn = MySQLdb.connect(
+    user="crawler",
+    passwd="crawler1937",
+    host="localhost",
+    db="crawling_test"
+)
+cursor = conn.cursor()
+
+# 실행할 때마다 다른값이 나오지 않게 테이블을 제거해두기
+cursor.execute("DROP TABLE IF EXISTS steam_best_popluar_genre")
+cursor.execute("DROP TABLE IF EXISTS steam_best_popluar")
+
+#steam_best 테이블 생성
+#rank에 AUTO_INCREMENT를 사용함으로써 INSERT가 입력될때마다 자동으로 숫자를 +1 올린다
+cursor.execute('''CREATE TABLE steam_best_popluar (
+                                           num int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                           title varchar(100) NOT NULL UNIQUE KEY,
+                                           price varchar(30) NOT NULL,
+                                           saleprice varchar(30),
+                                           saleper varchar(30),
+                                           description varchar(500),
+                                           imgdata varchar(3000)
+                                           )
+                                           ''')
+
+cursor.execute('''CREATE TABLE steam_best_popluar_genre (
+                                           num INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                           title varchar(100) NOT NULL,
+                                           genre varchar(20) NOT NULL,
+                                           FOREIGN KEY(title) REFERENCES steam_best_popluar(title)
+                                           )
+                                           ''')
 
 URL = 'https://store.steampowered.com/charts/topselling'
+
+services = Service(executable_path=ChromeDriverManager().install())
 
 options = Options()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=services, options=options)
 driver.set_window_size(1400,800)
 driver.get(URL)
-driver.implicitly_wait(10)
+driver.implicitly_wait(5)
 
 action = ActionChains(driver)
 
@@ -36,7 +73,7 @@ if steam_language.get_text() != "언어":
     driver.find_element(By.XPATH, '//*[@id="language_pulldown"]').click()
     driver.find_element(By.XPATH, '//*[@id="language_dropdown"]/div/a[4]').click()
 
-sleep(3)
+sleep(5)
 driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
 sleep(2) 
 
@@ -46,19 +83,9 @@ soup = BeautifulSoup(html, "html.parser")
 panel = soup.find('div', attrs={'data-featuretarget': 'react-root'})
 gamelist = panel.select('tr.weeklytopsellers_TableRow_2-RN6')
 
-excel_File = openpyxl.Workbook()
-excel_sheet = excel_File.active
 
-row_column = ["순위", "게임명", "할인가", "정가", "할인율", "파일 이미지"]
-excel_sheet.append(row_column)
-
-path = f'D:\Python\Study\SteamBest\{timestr}'
-Path(path).mkdir(parents=True, exist_ok=True)
-
-
-i = 1
 for item in gamelist:
-    title = item.select_one('div.weeklytopsellers_GameName_1n_4-')
+    title = item.select_one('div.weeklytopsellers_GameName_1n_4-').text
     saleprice = item.select_one('div.salepreviewwidgets_StoreSalePriceBox_Wh0L8')
     price = item.select_one('div.salepreviewwidgets_StoreOriginalPrice_1EKGZ')
     saleper = item.select_one('div.salepreviewwidgets_StoreSaleDiscountBox_2fpFv')
@@ -74,25 +101,135 @@ for item in gamelist:
     #정가가 None이면 if문 돌림
     if price == None:
         #saleprice 변수엔 "무료 플레이" 또는 가격 두가지 값이 들어갈수있음
-        #if문으로 saleprice(현재 판매가) 값이 "무료 플레이"가 아니면 price(정가)는 현재 판매가랑 같은 값을 갖게됨
-        if saleprice != "무료 플레이":
+        #if문으로 saleprice(현재 판매가) 값이 "무료 플레이"가 아니면 price(정가)는 현재 판매가랑같은 값을 갖게됨(할인을 안하는중이라)
+        if saleprice == None:
+            price = "X"
+            saleper = "X"
+            saleprice = "X"
+        elif saleprice != "무료 플레이":
             price = saleprice.text
             saleper = "X"
+            saleprice = "X"
         #else문으로 saleprice값이 "무료 플레이"면 정가와 할인율을 무료로 변경
         else:
             price = "무료"
             saleper = "무료"
+            saleprice = "무료"
     #정가가 적혀있으면 정가와 할인율이 둘다 문자열로 존재할테니 price, saleper 변수를 문자열 자료형으로 변환
     else:
         price = price.text
         saleper = saleper.text
+        saleprice = saleprice.text
+        
+    #move 변수에 해당 게임 페이지 링크 획득
+    game_link = item.select_one('a.weeklytopsellers_TopChartItem_2C5PJ')
+    move = game_link["href"]
+    response = requests.get(move, cookies={'Steam_Language':'koreana'}).text
 
-    data_column = [i, title.text, saleprice.text, price, saleper, imgdata]
-    excel_sheet.append(data_column)
-    dload.save(imgdata, f'{path}\IMG_{i}.jpg')
+    """
+    #게임 페이지로 새탭에서 열기
+    driver.execute_script(f'window.open(\'{move}\');')
+    #새탭으로 이동
+    driver.switch_to.window(driver.window_handles[-1])
+    sleep(1)
+
+    #해당탭 크롤링
+    new_html = driver.page_source
+    """
+    new_soup = BeautifulSoup(response, "html.parser")
+
+    """
+    check = new_soup.select_one("div.agegate_btn_ctn")
+    if check != None:
+        driver.find_element(By.XPATH, '//*[@id="ageYear"]').click()
+        driver.find_element(By.XPATH, '//*[@id="ageYear"]/option[88]').click()
+        driver.find_element(By.XPATH, '//*[@id="view_product_page_btn"]/span').click()
+        sleep(1)
+        new_html = driver.page_source
+        new_soup = BeautifulSoup(new_html, "html.parser")
+        """
+
+    description = new_soup.select_one("div.game_description_snippet")
+    if description != None:
+        description = description.text.strip()
+    tag = new_soup.select("a.app_tag")
+    
+    tag_length = len(tag)
+    num = 0
+
+    sql = 'INSERT INTO steam_best_popluar (title, price, saleprice, saleper, description, imgdata) VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(sql, (title, price, saleprice, saleper, description, imgdata))
+
+    while num < tag_length:
+        tag[num] = tag[num].text.strip()
+        sql = 'INSERT INTO steam_best_popluar_genre (title, genre) VALUES (%s, %s)'
+        cursor.execute(sql, (title, tag[num]))
+        num += 1
+
+    conn.commit()
+    print(f'{title} db에 입력완료')
+
+    #탭 종료후 원래 탭(베스트게임 페이지)으로 이동
+    #driver.close()
+    #driver.switch_to.window(driver.window_handles[-1])
+
+sleep(2)
+print("크롤링 완료")
+driver.quit()
+
+
+#excel_File = openpyxl.Workbook()
+#excel_sheet = excel_File.active
+
+#row_column = ["순위", "게임명", "할인가", "정가", "할인율", "파일 이미지"]
+#excel_sheet.append(row_column)
+
+#path = f'D:\Python\Study\SteamBest\{timestr}'
+#Path(path).mkdir(parents=True, exist_ok=True)
+
+"""
+i = 1
+for item in gamelist:
+    title = item.select_one('div.weeklytopsellers_GameName_1n_4-').text
+    saleprice = item.select_one('div.salepreviewwidgets_StoreSalePriceBox_Wh0L8')
+    price = item.select_one('div.salepreviewwidgets_StoreOriginalPrice_1EKGZ')
+    saleper = item.select_one('div.salepreviewwidgets_StoreSaleDiscountBox_2fpFv')
+    img = item.select_one('img.weeklytopsellers_CapsuleArt_2dODJ')
+
+    #이미지 태그가 존재하면 imgdata에 img 링크 집어넣음
+    if img != None:
+        imgdata = img["src"]
+    #이미지 태그가 존재하지않으면 imgdata에 다른 임시 이미지 집어넣음
+    else:
+        imgdata = "https://i.namu.wiki/i/j_EaOmOmU8QGpxXHpZGA75dSasZthOT5_X_nlFjUO3VwaxHuf0f5_0h0yKM8I65d9Jxwe74ynwTZRLfjSL8Yk0QCt871C6A-H-76SqzQ47QO2zGVf-6MJCITrIlds4vpPaG1fmZU3Ppyv12nf803tg.webp"
+
+    #정가가 None이면 if문 돌림
+    if price == None:
+        #saleprice 변수엔 "무료 플레이" 또는 가격 두가지 값이 들어갈수있음
+        #if문으로 saleprice(현재 판매가) 값이 "무료 플레이"가 아니면 price(정가)는 현재 판매가랑같은 값을 갖게됨(할인을 안하는중이라)
+        if saleprice != "무료 플레이":
+            price = saleprice.text
+            saleper = "X"
+            saleprice = "X"
+        #else문으로 saleprice값이 "무료 플레이"면 정가와 할인율을 무료로 변경
+        else:
+            price = "무료"
+            saleper = "무료"
+            saleprice = "무료"
+    #정가가 적혀있으면 정가와 할인율이 둘다 문자열로 존재할테니 price, saleper 변수를 문자열 자료형으로 변환
+    else:
+        price = price.text
+        saleper = saleper.text
+        saleprice = saleprice.text
+
+    print(f'{i}.{title}, 정가:{price}, 할인가:{saleprice}, 할인율:{saleper}, 이미지링크:{imgdata}')
+    #excel_sheet.append(data_column)
+    #dload.save(imgdata, f'{path}\IMG_{i}.jpg')
     i += 1
-excel_File.save(f'{path}\CrollingTest_{timestr}.xlsx')
-excel_File.close()
+#excel_File.save(f'{path}\CrollingTest_{timestr}.xlsx')
+#excel_File.close()
+
 
 sleep(5)
 driver.quit()
+"""
