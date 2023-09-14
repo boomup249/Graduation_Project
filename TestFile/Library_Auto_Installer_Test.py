06a79ab9cefc04db54c2f69f0707f1638035d11d
yofp1937 <yofp1937@naver.com>
2023-09-15 03:30:12 +0900

    라이브러리 자동 설치 테스트 파일 완성

--- TestFile/Library_Auto_Installer_Test.py
+++ TestFile/Library_Auto_Installer_Test.py
@@ -1,15 +1,46 @@
 import importlib
 
+from time import sleep
 def install_and_import(package):
     try:
         importlib.import_module(package)
     except ImportError:
         import subprocess
+        print(package, " installing...")
         subprocess.check_call(["pip", "install", package])
-    finally:
-        globals()[package] = importlib.import_module(package)
+        sleep(2)
 
 try:
     import requests
 except ImportError:
     install_and_import("requests")
+
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    install_and_import("bs4")
+
+try:
+    import selenium
+except ImportError:
+    install_and_import("selenium")
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+try:
+    from webdriver_manager.chrome import ChromeDriverManager
+except ImportError:
+    install_and_import("webdriver-manager")
+
+try:
+    import MySQLdb
+except ImportError:
+    install_and_import("mysqlclient")
+
+print("필수 라이브러리 인스톨 완료")
