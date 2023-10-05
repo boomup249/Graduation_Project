97ad7ff526b4f33427dca58acb661fe9181d4b1b
boomup249 <boomup249@naver.com>
2023-10-06 05:38:48 +0900

    Create DB(프로필, 게시판 쪽 분리).sql

---
+++ sql/DB(프로필, 게시판 쪽 분리).sql
@@ -0,0 +1,88 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema member
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema member
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `member` DEFAULT CHARACTER SET utf8mb3 ;
+USE `member` ;
+
+-- -----------------------------------------------------
+-- Table `member`.`bbs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `member`.`bbs` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `TITLE` VARCHAR(20) NOT NULL,
+  `WRITER` VARCHAR(12) NOT NULL,
+  `CATEGORY` VARCHAR(10) NULL DEFAULT NULL,
+  `DATE` VARCHAR(30) NOT NULL,
+  `VIEWS` INT NULL DEFAULT NULL,
+  `COMMENT` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `ID_idx` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `member`.`bbs_content`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `member`.`bbs_content` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `CATEGORY` VARCHAR(10) NOT NULL,
+  `TITLE` VARCHAR(20) NOT NULL,
+  `WRITER` VARCHAR(12) NOT NULL,
+  `CONTENT` TEXT NULL DEFAULT NULL,
+  `COMMENT` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `uploader_idx` (`WRITER` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `member`.`info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `member`.`info` (
+  `ID` VARCHAR(12) NOT NULL,
+  `EMAIL` VARCHAR(30) NULL DEFAULT NULL,
+  `PWD` CHAR(100) NOT NULL,
+  `BIRTH` DATE NULL DEFAULT NULL,
+  `GENDER` CHAR(5) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `member`.`prefer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `member`.`prefer` (
+  `ID` VARCHAR(12) NOT NULL,
+  `love` VARCHAR(300) NULL DEFAULT NULL,
+  `dislike` VARCHAR(300) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `preferID`
+    FOREIGN KEY (`ID`)
+    REFERENCES `member`.`info` (`ID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
